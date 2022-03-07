b68f8f91cbe0f4952568a8213e330dbe2e68857c
Amarnath0510 <amarmuthu007@gmail.com>
2022-03-07 10:26:31 +0530

    mongo commands added

--- index.js
+++ index.js
@@ -1,161 +1,189 @@
 import express from "express";
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 // import mongodb from "mongodb";
-const app=express();
-const PORT=5000;
-const users=[
-    {
-      "name": "Scarlett",
-      "avatar": "https://www.microsoft.com/insidetrack/blog/uploads/prod/2018/06/IMG_1786.jpg",
-      "id": "9",
-      "profession": "Software Engineer",
-      "place": "New York",
-      "quotes": "Be silent and achieve"
-    },
-    {
-      "name": "Franklin",
-      "avatar": "https://www.pngitem.com/pimgs/m/455-4554869_doctor-with-stethoscope-png-png-download-doctor-avatars.png",
-  
-      "id": "10",
-      "profession": "Doctor",
-      "place": "California",
-      "quotes": "Never lose sight of the face that the most important yard stick to your success is how you treat other people"
-    },
-    {
-      "name": "Catherine",
-      "avatar": "https://careerwise.ceric.ca/wp-content/uploads/2020/11/iStock-1226295217.jpg",
-      "id": "11",
-      "profession": "Mentor",
-      "place": "Paris",
-      "quotes": "You know you're in love when you can't fall asleep because reality is finally better than your dreams."
-    },
-    {
-      "name": "Sylvester",
-      "avatar": "https://www.simplilearn.com/ice9/free_resources_article_thumb/What-Skills-Do-I-Need-to-Become-a-Data-Scientist.jpg",
-      "id": "12",
-      "profession": "Data Scientist",
-      "place": "Florida",
-      "quotes": "In three words I can sum up everything I've learned about life: it goes on."
-    },
-    {
-      "name": "Benzia",
-      "avatar": "https://www.kindpng.com/picc/m/193-1933797_girl-png-avatars-welcome-air-hostess-png-transparent.png",
-      "id": "13",
-      "profession": "Air Hostess",
-      "place": "London",
-      "quotes": "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."
-    },
-    {
-      "name": "Williams",
-      "avatar": "https://bentech.com.au/wp-content/uploads/2015/10/dt.common.streams.StreamServer-e1494831911548.jpg",
-      "id": "14",
-      "profession": "Chartered Accountant",
-      "place": "Washington",
-      "quotes": "If you tell the truth, you don't have to remember anything."
-    },
-    {
-      "name": "Adams",
-      "avatar": "https://media.istockphoto.com/photos/corporate-pilot-using-electronic-tablet-picture-id163221634?k=20&m=163221634&s=612x612&w=0&h=TavPJ_uswmhyw7n4H2Q3LYQxVMn_J31jQu1phUN6SBs=",
-      "id": "15",
-      "profession": "Commercial pilot",
-      "place": "Beijing",
-      "quotes": "A friend is someone who knows all about you and still loves you."
-    },
-    {
-      "name": "Ramsay",
-      "avatar": "https://static.cordonbleu.edu/Files/MediaFile/70794.jpg",
-      "id": "16",
-      "profession": "Executive-chef",
-      "place": "Mexico",
-      "quotes": "To live is the rarest thing in the world. Most people exist, that is all."
-    },
-    {
-      "name": "Brewson",
-      "avatar": "https://media.istockphoto.com/photos/businessman-signing-electronic-legal-document-on-digital-tablet-picture-id1293402642?b=1&k=20&m=1293402642&s=170667a&w=0&h=u86msOQpUVkGj91eHnav8_YolPjmhBWBoZeq1UXCTU4=",
-      "id": "17",
-      "profession": "Legal Advisor",
-      "place": "Sydney",
-      "quotes": "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment."
-    },
-    {
-      "name": "Hemsworth",
-      "avatar": "https://torontolife.com/wp-content/uploads/2021/05/USED-DSC07686-copy-803x603.jpg",
-      "id": "18",
-      "profession": "Entrepreneur",
-      "place": "Frankfurt",
-      "quotes": "The greatest glory in living lies not in never falling, but in rising every time we fall."
-    },
-    {
-      "name": "Denverson",
-      "avatar": "https://www.bmwgroup.jobs/content/dam/grpw/websites/bmwgroup_jobs/cn/all-jobfields/engineering/BMW-Careers-engineering-engineer-laptop-car-hm.jpg.grp-transform/small/BMW-Careers-engineering-engineer-laptop-car-hm.jpg",
-      "id": "19",
-      "profession": "Automobile Engineer",
-      "place": "Toronto",
-      "quotes": "The way to get started is to quit talking and begin doing"
-    },
-    {
-      "name": "Jack",
-      "avatar": "https://snowmaster.com.au/wp-content/uploads/2019/11/Own-a-Restaurent.jpg",
-      "id": "20",
-      "profession": "Restaurant owner",
-      "place": "Texas",
-      "quotes": "If life were predictable it would cease to be life, and be without flavor."
-    }
-  ]
- 
-  const MONGO_URL="mongodb://localhost";
-  async function createConnection(){
-    const client=new MongoClient(MONGO_URL);
-    await client.connect();
-    console.log("Mongodb connected succcesfully");
-    return client;
-  }
-  const client = await createConnection();
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+const users = [
+  {
+    name: "Scarlett",
+    avatar:
+      "https://www.microsoft.com/insidetrack/blog/uploads/prod/2018/06/IMG_1786.jpg",
+    id: "9",
+    profession: "Software Engineer",
+    place: "New York",
+    quotes: "Be silent and achieve",
+  },
+  {
+    name: "Franklin",
+    avatar:
+      "https://www.pngitem.com/pimgs/m/455-4554869_doctor-with-stethoscope-png-png-download-doctor-avatars.png",
 
+    id: "10",
+    profession: "Doctor",
+    place: "California",
+    quotes:
+      "Never lose sight of the face that the most important yard stick to your success is how you treat other people",
+  },
+  {
+    name: "Catherine",
+    avatar:
+      "https://careerwise.ceric.ca/wp-content/uploads/2020/11/iStock-1226295217.jpg",
+    id: "11",
+    profession: "Mentor",
+    place: "Paris",
+    quotes:
+      "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+  },
+  {
+    name: "Sylvester",
+    avatar:
+      "https://www.simplilearn.com/ice9/free_resources_article_thumb/What-Skills-Do-I-Need-to-Become-a-Data-Scientist.jpg",
+    id: "12",
+    profession: "Data Scientist",
+    place: "Florida",
+    quotes:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+  },
+  {
+    name: "Benzia",
+    avatar:
+      "https://www.kindpng.com/picc/m/193-1933797_girl-png-avatars-welcome-air-hostess-png-transparent.png",
+    id: "13",
+    profession: "Air Hostess",
+    place: "London",
+    quotes:
+      "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+  },
+  {
+    name: "Williams",
+    avatar:
+      "https://bentech.com.au/wp-content/uploads/2015/10/dt.common.streams.StreamServer-e1494831911548.jpg",
+    id: "14",
+    profession: "Chartered Accountant",
+    place: "Washington",
+    quotes: "If you tell the truth, you don't have to remember anything.",
+  },
+  {
+    name: "Adams",
+    avatar:
+      "https://media.istockphoto.com/photos/corporate-pilot-using-electronic-tablet-picture-id163221634?k=20&m=163221634&s=612x612&w=0&h=TavPJ_uswmhyw7n4H2Q3LYQxVMn_J31jQu1phUN6SBs=",
+    id: "15",
+    profession: "Commercial pilot",
+    place: "Beijing",
+    quotes: "A friend is someone who knows all about you and still loves you.",
+  },
+  {
+    name: "Ramsay",
+    avatar: "https://static.cordonbleu.edu/Files/MediaFile/70794.jpg",
+    id: "16",
+    profession: "Executive-chef",
+    place: "Mexico",
+    quotes:
+      "To live is the rarest thing in the world. Most people exist, that is all.",
+  },
+  {
+    name: "Brewson",
+    avatar:
+      "https://media.istockphoto.com/photos/businessman-signing-electronic-legal-document-on-digital-tablet-picture-id1293402642?b=1&k=20&m=1293402642&s=170667a&w=0&h=u86msOQpUVkGj91eHnav8_YolPjmhBWBoZeq1UXCTU4=",
+    id: "17",
+    profession: "Legal Advisor",
+    place: "Sydney",
+    quotes:
+      "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+  },
+  {
+    name: "Hemsworth",
+    avatar:
+      "https://torontolife.com/wp-content/uploads/2021/05/USED-DSC07686-copy-803x603.jpg",
+    id: "18",
+    profession: "Entrepreneur",
+    place: "Frankfurt",
+    quotes:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+  },
+  {
+    name: "Denverson",
+    avatar:
+      "https://www.bmwgroup.jobs/content/dam/grpw/websites/bmwgroup_jobs/cn/all-jobfields/engineering/BMW-Careers-engineering-engineer-laptop-car-hm.jpg.grp-transform/small/BMW-Careers-engineering-engineer-laptop-car-hm.jpg",
+    id: "19",
+    profession: "Automobile Engineer",
+    place: "Toronto",
+    quotes: "The way to get started is to quit talking and begin doing",
+  },
+  {
+    name: "Jack",
+    avatar:
+      "https://snowmaster.com.au/wp-content/uploads/2019/11/Own-a-Restaurent.jpg",
+    id: "20",
+    profession: "Restaurant owner",
+    place: "Texas",
+    quotes:
+      "If life were predictable it would cease to be life, and be without flavor.",
+  },
+];
+const MONGO_URL = "mongodb://localhost";
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongodb connected succcesfully");
+  return client;
+}
+const client = await createConnection();
 
-app.get("/",(request,response)=>{
-    response.send("HeY Welcome you all to users Palace🏛🏛😍👩‍👩‍👧‍👦👨‍👧‍👧👨🏽‍🤝‍👨🏽🤩");
+app.get("/", (request, response) => {
+  response.send("HeY Welcome you all to users Palace🏛🏛😍👩‍👩‍👧‍👦👨‍👧‍👧👨🏽‍🤝‍👨🏽🤩");
+});
+
+app.get("/users",async (request, response) => {
+  console.log(request.query);
+  const filter=request.query;
+  console.log(filter);
+  // const { name, profession, place, quotes } = request.query;
+  const filterUsers= await client
+  .db("users")
+  .collection("users")
+  .find(filter)
+  .toArray();
+  // console.log(name, profession, place);
+  // let filterUsers = users;
+  // if (name) {
+  //   filterUsers = filterUsers.filter((ur) => ur.name === name);
+  // }
+  // if (profession) {
+  //   filterUsers = filterUsers.filter((ur) => ur.profession === profession);
+  // }
+  // if (place) {
+  //   filterUsers = filterUsers.filter((ur) => ur.place === place);
+  // }
+
+  // if (quotes) {
+  //   filterUsers = filterUsers.filter((ur) => ur.quotes === quotes);
+  // }
+  response.send(filterUsers);
+});
+
+app.get("/users/:id", async (request, response) => {
+  console.log(request.params);
+  const { id } = request.params;
+  // const user = users.find((ur)=> ur.id===id);
+  const user = await client.db("users").collection("users").findOne({ id: id });
+  console.log(user);
+  user
+    ? response.send(user)
+    : response.status(404).send({ messasge: "No matchung user found" });
 });
 
 
-app.get("/users",(request,response)=>{
-    console.log(request.query);
-    const{name,profession,place,quotes}=request.query;
-    console.log(name,profession,place);
-    let filterUsers=users;
-    if(name){
-        filterUsers=filterUsers.filter((ur)=>ur.name===name);
-    }
-    if(profession){
-        filterUsers=filterUsers.filter((ur)=>ur.profession===profession);
-    }
-    if(place){
-        filterUsers=filterUsers.filter((ur)=>ur.place===place);
-    }
-    
-    if(quotes){
-        filterUsers=filterUsers.filter((ur)=>ur.quotes===quotes);
-    }
-    response.send(filterUsers);
-});
+app.post("/users",async(request,response)=>{
+  const data=request.body;
+  const result=await client
+  .db("users")
+  .collection("users")
+  .insertMany(data)
 
-
-app.get("/users/:id",async(request,response)=>{
-    console.log(request.params);
-    const  {id}=request.params;
-    // const user = users.find((ur)=> ur.id===id);
-    const user= await client
-    .db("users")
-    .collection("users")
-    .findOne({id:id});
-    console.log(user);
-    user
-    ?response.send(user)
-    :response.status(404).send({messasge:"No matchung user found"});
-
-});
-
-
-app.listen(PORT,()=>console.log("App is started in",PORT))
+  response.send(result);
+})
+app.listen(PORT, () => console.log("App is started in", PORT));
 
 
